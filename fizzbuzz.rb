3bef665c08d86380f71020b581d6b21742491b70
GraceAHerr <mazyg321@gmail.com>
2018-06-19 19:49:39 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,21 @@
+
+
+def fizz_buzz(int)
+  if int%15 == 0
+    puts "Fizz_Buzz"
+
+  elsif int%5 == 0
+    puts "Buzz"
+
+elsif int%3 == 0
+  puts "Fizz"
+
+else
+  puts "nil"
+  end
+end
+
+fizz_buzz(5)
+fizz_buzz(3)
+fizz_buzz(15)
+fizz_buzz(4)
